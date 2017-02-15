b1678d8da82a782bed46856bf28f8a09c105d610
WhiteTrefoil <whitetrefoil@gmail.com>
2017-02-15 17:32:20 +0800

    Dynamically show login / logout in nav

--- src/components/wt-main-nav/index.ts
+++ src/components/wt-main-nav/index.ts
@@ -1,7 +1,14 @@
 import { Component, Vue } from 'av-ts'
+import * as _             from 'lodash'
+import { store }          from '../../store'
 
 @Component({
   name: 'wt-main-nav',
 })
 export default class WtMainNav extends Vue {
+
+  get hasLoggedIn(): boolean {
+    return !_.isEmpty(_.get(store.state, 'login.token'))
+      && !_.isEmpty(_.get(store.state, 'login.user'))
+  }
 }
